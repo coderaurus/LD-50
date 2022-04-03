5bd55e0e473b3b8b832875a66432af9df510e6a1
CodeRaurus <coderaurus@gmail.com>
2022-04-03 12:01:39 +0300

    Change health to hide UI at ready and play pop in all hit cases (even if parent dies)

--- scenes/Health.gd
+++ scenes/Health.gd
@@ -12,17 +12,20 @@ var alive : bool = true
 func _ready():
 	current_hp = max_hp
 	$Health2.text = str(current_hp)
+	$AnimationPlayer.play("Hide")
 
 func _on_Health_hit():
+	print("%s got hit" % get_parent().name)
 	current_hp -= 1
 	$Health2.text = str(current_hp)
+	
+	$AnimationPlayer.play("Pop")
 	
 #	print("%s has %s hp" % [get_parent().name, current_hp])
 	if current_hp == 0:
 		alive = false
 		get_parent().emit_signal("dead")
-	else:
-		$AnimationPlayer.play("Pop")
+	
 
 func alive():
 #	print("%s alive: %s" % [get_parent().name, alive])
